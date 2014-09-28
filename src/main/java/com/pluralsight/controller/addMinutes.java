9ad705487e6ae123c50219267f7cf9555d3e38dc
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-09-28 22:32:36 +0600

    forwarding view

--- src/main/java/com/pluralsight/controller/addMinutes.java
+++ src/main/java/com/pluralsight/controller/addMinutes.java
@@ -14,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class addMinutes {
 
     @RequestMapping(value = "/addMinutes")
-    public String addMinutes(@ModelAttribute("exercise")Exercise exercise)
+    public String addMinutes(@ModelAttribute("exercise") Exercise exercise)
     {
-        System.out.println(exercise.getMinutes());
+        System.out.println("exercise: " + exercise.getMinutes());
+
+        return "forward:addMoreMinutes.html";
+    }
+
+    @RequestMapping(value = "/addMoreMinutes")
+    public String addMoreMinutes(@ModelAttribute("exercise")Exercise exercise)
+    {
+        System.out.println("exercising: " + exercise.getMinutes());
 
         return "addMinutes";
     }
