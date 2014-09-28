70f291d763d68759834325ec63a77f74b8da04c3
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-09-28 09:59:06 +0600

    initcommit

---
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jahangiralamdiu on 28/09/2014.
+ */
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/greeting")
+    public String sayHally(Model model)
+    {
+        model.addAttribute("greeting", "Hello Jahangir");
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jahangiralamdiu
+  Date: 28/09/2014
+  Time: 09:50 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>${greeting}</h1>
+</body>
+</html>
