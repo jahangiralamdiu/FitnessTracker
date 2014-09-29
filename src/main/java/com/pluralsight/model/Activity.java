25cb3360584a4fa96a97e89a1931d548d39b01e0
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-09-29 19:27:43 +0600

    creating json output

---
+++ src/main/java/com/pluralsight/model/Activity.java
@@ -0,0 +1,17 @@
+package com.pluralsight.model;
+
+/**
+ * Created by lenovo on 29/09/2014.
+ */
+public class Activity {
+
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
